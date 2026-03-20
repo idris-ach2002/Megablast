5fd4397a64219c288318e6befb36dc9a6c6f52a4
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-03-20 21:41:19 +0100

    Implémentation des fonctionnalités du Modèle '1, 3, 5' [IDRIS]

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,348 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+{- HLINT ignore "Redundant bracket" -}
+{- HLINT ignore "Eta reduce" -}
+{- HLINT ignore "Use record patterns" -}
+{- HLINT ignore "Use camelCase" -}
 
--- Le modèle de Megablast
+module Lib
+  ( someFunc
+  -- Utilitaires
+  , tshow
+
+  -- Partie 1 : Hitbox
+  , Hitbox(..)
+  , prop_inv_hitbox
+  , mkPoint, mkDisque, mkRectangle, mkComposee
+  , collision
+  , prop_composee2points_point
+
+  -- Partie 3 : Obstacles / Projectiles / Tour
+  , Direction(..), dirVector
+  , Cadence(..), prop_inv_cadence, mkCadence, tickCadence
+  , prop_pre_tickCadence, prop_post_tickCadence
+  , Obstacle(..), prop_inv_obstacle, mkObstacle, defileObstacle
+  , ProjectileOwner(..)
+  , Projectile(..), prop_inv_projectile, mkProjectile
+  , avanceProjectile, prop_pre_avanceProjectile, prop_post_avanceProjectile
+  , finDeTourProjectile, prop_pre_finDeTourProjectile, prop_post_finDeTourProjectile
+  , finDeTourObstacles, prop_pre_finDeTourObstacles, prop_post_finDeTourObstacles
+
+  -- Partie 5 : Ennemis
+  , Action(..)
+  , Oracle(..), oracleStep, prop_pre_oracleStep, prop_post_oracleStep
+  , PV(..), prop_inv_pv, mkPV
+  , Ennemi(..), prop_inv_ennemi, mkEnnemi
+  , finDeTourEnnemi, prop_pre_finDeTourEnnemi, prop_post_finDeTourEnnemi
+  ) where
+
+import Data.Text (Text)
+import qualified Data.Text as T
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
--- modèle d'un pixel, fenêtre hitbox
--- hitbox : c’est un ensemble de pixels qui repréesentent la surface totale couverte par l’objet.
-data Pixel = Pixel {pixel_x :: Int , pixel_y :: Int} deriving (Show, Eq, Ord)
-data Pane = Pane {largeur:: Int, hauteur :: Int} deriving (Show, Eq)
+tshow :: Show a => a -> Text
+tshow = T.pack . show
 
--- Taille de la fenêtre par défaut 800 x 500
-main_pane :: Pane
-main_pane = Pane 800 500
+--------------------------------------------------------------------------------
+-- Partie 1 : Hitbox
+--------------------------------------------------------------------------------
 
-data Hitbox = Point Int Int
-    | Disque Int Int Int
-    | Rectangle Int Int Int Int
-    | Composee [Hitbox] 
-    deriving (Eq, Show)
+data Hitbox
+  = Point Int Int
+  | Disque Int Int Int
+  | Rectangle Int Int Int Int
+  | Composee [Hitbox]
+  deriving (Eq, Show)
 
+-- Invariant structurel
 prop_inv_hitbox :: Hitbox -> Bool
-prop_inv_hitbox (Point x y) = x >= 0 && x <= (largeur main_pane) && y >= 0 && y <= (hauteur main_pane)
-prop_inv_hitbox (Disque xc yc r) = r > 0 && (xc - r) >= 0 && (xc + r) <= (largeur main_pane) && (yc - r) >= 0 && (yc + r) <= (hauteur main_pane)
+prop_inv_hitbox (Point _ _)         = True
+prop_inv_hitbox (Disque _ _ r)      = r > 0
+prop_inv_hitbox (Rectangle _ _ w h) = w > 0 && h > 0
+prop_inv_hitbox (Composee hs)       = length hs >= 2 && all prop_inv_hitbox hs
+
+-- Smart constructors
+mkPoint :: Int -> Int -> Hitbox
+mkPoint x y = Point x y
+
+mkDisque :: Int -> Int -> Int -> Either Text Hitbox
+mkDisque xc yc r
+  | r > 0     = Right (Disque xc yc r)
+  | otherwise = Left ("Rayon non strictement positif: r=" <> tshow r)
+
+mkRectangle :: Int -> Int -> Int -> Int -> Either Text Hitbox
+mkRectangle x y w h
+  | w > 0 && h > 0 = Right (Rectangle x y w h)
+  | otherwise      = Left ("Rectangle invalide: w=" <> tshow w <> ", h=" <> tshow h)
+
+mkComposee :: [Hitbox] -> Either Text Hitbox
+mkComposee hs
+  | length hs < 2          = Left "Une hitbox Composee doit avoir au moins 2 composantes"
+  | all prop_inv_hitbox hs = Right (Composee hs)
+  | otherwise              = Left "Une composante viole prop_inv_hitbox"
+
+-- Collisions : cas demandés dans l'ER1 (et on rend le reste False, pas undefined)
+collision :: Hitbox -> Hitbox -> Bool
+collision (Point x1 y1) (Point x2 y2) =
+  x1 == x2 && y1 == y2
+
+collision (Rectangle x1 y1 w1 h1) (Rectangle x2 y2 w2 h2) =
+  x1 < x2 + w2 && x2 < x1 + w1 && y1 < y2 + h2 && y2 < y1 + h1
+
+collision (Point x y) (Disque xc yc r) =
+  let dx = fromIntegral (x - xc) :: Float
+      dy = fromIntegral (y - yc) :: Float
+  in sqrt (dx*dx + dy*dy) <= fromIntegral r
+collision d@(Disque _ _ _) p@(Point _ _) = collision p d
+
+collision (Composee hs) h = any (`collision` h) hs
+collision h (Composee hs) = any (collision h) hs
+
+collision _ _ = False
+
+-- Q1.4 : si h1 = Composee [p1,p2] et h2 est un Point en collision avec h1,
+-- alors h2 est p1 ou p2.
+prop_composee2points_point :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Bool
+prop_composee2points_point (x1,y1) (x2,y2) (x,y) =
+  let p1 = Point x1 y1
+      p2 = Point x2 y2
+      h1 = Composee [p1,p2]
+      h2 = Point x y
+  in collision h1 h2 ==> (h2 == p1 || h2 == p2)
+  where
+    (==>) a b = (not a) || b
+
+--------------------------------------------------------------------------------
+-- Partie 3 : Obstacles / Projectiles / Tour
+--------------------------------------------------------------------------------
+
+data Direction = Haut | Bas | Gauche | Droite
+  deriving (Eq, Show, Enum, Bounded)
+
+dirVector :: Direction -> (Int, Int)
+dirVector Haut   = (0, 1)
+dirVector Bas    = (0, -1)
+dirVector Gauche = (-1, 0)
+dirVector Droite = (1, 0)
+
+-- Cadence: attente>0, restant ∈ [0..attente-1], restant==0 => bouge maintenant
+-- attente = N (tous les combien de tours on bouge)
+--restant = compteur interne “combien de tours avant le prochain mouvement”
+data Cadence = Cadence { attente :: Int, restant :: Int }
+  deriving (Eq, Show)
+
+prop_inv_cadence :: Cadence -> Bool
+prop_inv_cadence (Cadence a r) = a > 0 && 0 <= r && r < a
+
+mkCadence :: Int -> Either Text Cadence
+mkCadence a
+  | a > 0     = Right (Cadence a 0)
+  | otherwise = Left ("attente doit être > 0, reçu " <> tshow a)
+
+-- si restant == 0:
+-- on doit bouger ce tour-ci (True)
+-- puis on remet le compteur à a-1 (il faudra attendre a-1 tours avant le prochain mouvement)
+tickCadence :: Cadence -> (Bool, Cadence)
+tickCadence c@(Cadence a r)
+  | not (prop_inv_cadence c) = error "tickCadence: invariant cadence violé"
+  | r == 0    = (True,  Cadence a (a - 1))
+  | otherwise = (False, Cadence a (r - 1))
+
+prop_pre_tickCadence :: Cadence -> Bool
+prop_pre_tickCadence = prop_inv_cadence
+
+prop_post_tickCadence :: Cadence -> (Bool, Cadence) -> Bool
+prop_post_tickCadence (Cadence a r) (moveNow, Cadence a' r') =
+  a' == a
+  && prop_inv_cadence (Cadence a' r')
+  && if r == 0
+       then moveNow && r' == a - 1
+       else (not moveNow) && r' == r - 1
+
+translateHitbox :: Int -> Int -> Hitbox -> Hitbox
+translateHitbox dx dy (Point x y)            = Point (x + dx) (y + dy)
+translateHitbox dx dy (Disque xc yc r)       = Disque (xc + dx) (yc + dy) r
+translateHitbox dx dy (Rectangle x y w h)    = Rectangle (x + dx) (y + dy) w h
+translateHitbox dx dy (Composee hs)          = Composee (map (translateHitbox dx dy) hs)
+
+newtype Obstacle = Obstacle { obsHitbox :: Hitbox }
+  deriving (Eq, Show)
+
+prop_inv_obstacle :: Obstacle -> Bool
+prop_inv_obstacle (Obstacle h) = prop_inv_hitbox h
+
+mkObstacle :: Hitbox -> Either Text Obstacle
+mkObstacle h
+  | prop_inv_hitbox h = Right (Obstacle h)
+  | otherwise         = Left "Hitbox invalide pour obstacle"
+
+defileObstacle :: Obstacle -> Obstacle
+defileObstacle (Obstacle h) = Obstacle (translateHitbox 0 (-1) h)
+
+data ProjectileOwner = TirJoueuse | TirEnnemi
+  deriving (Eq, Show, Enum, Bounded)
+
+data Projectile = Projectile
+  { prHitbox  :: Hitbox
+  , prDir     :: Direction
+  , prCadence :: Cadence
+  , prOwner   :: ProjectileOwner
+  } deriving (Eq, Show)
+
+prop_inv_projectile :: Projectile -> Bool
+prop_inv_projectile p =
+  prop_inv_hitbox (prHitbox p) && prop_inv_cadence (prCadence p)
+
+mkProjectile :: Hitbox -> Direction -> Cadence -> ProjectileOwner -> Either Text Projectile
+mkProjectile h d c o
+  | prop_inv_hitbox h && prop_inv_cadence c = Right (Projectile h d c o)
+  | otherwise = Left "Projectile invalide (hitbox ou cadence)"
+
+avanceProjectile :: Projectile -> Projectile
+avanceProjectile p =
+  let (dx, dy) = dirVector (prDir p)
+  in p { prHitbox = translateHitbox dx dy (prHitbox p) }
+
+prop_pre_avanceProjectile :: Projectile -> Bool
+prop_pre_avanceProjectile = prop_inv_projectile
+
+prop_post_avanceProjectile :: Projectile -> Projectile -> Bool
+prop_post_avanceProjectile p p' =
+  prop_inv_projectile p'
+  && prDir p' == prDir p
+  && prCadence p' == prCadence p
+  && prOwner p' == prOwner p
+
+finDeTourProjectile :: Projectile -> Projectile
+finDeTourProjectile p
+  | not (prop_inv_projectile p) = error "finDeTourProjectile: invariant projectile violé"
+  | otherwise =
+      let (moveNow, c') = tickCadence (prCadence p)
+          p' = p { prCadence = c' }
+      in if moveNow then avanceProjectile p' else p'
+
+prop_pre_finDeTourProjectile :: Projectile -> Bool
+prop_pre_finDeTourProjectile = prop_inv_projectile
+
+prop_post_finDeTourProjectile :: Projectile -> Projectile -> Bool
+prop_post_finDeTourProjectile p p' =
+  prop_inv_projectile p'
+  && prDir p' == prDir p
+  && prOwner p' == prOwner p
+
+-- scrolling global: on tick une cadence, si moveNow alors tous les obstacles descendent
+finDeTourObstacles :: Cadence -> [Obstacle] -> (Cadence, [Obstacle])
+finDeTourObstacles cad obs
+  | not (prop_inv_cadence cad) = error "finDeTourObstacles: invariant cadence violé"
+  | otherwise =
+      let (moveNow, cad') = tickCadence cad
+          obs' = if moveNow then map defileObstacle obs else obs
+      in (cad', obs')
+
+prop_pre_finDeTourObstacles :: Cadence -> [Obstacle] -> Bool
+prop_pre_finDeTourObstacles cad obs =
+  prop_inv_cadence cad && all prop_inv_obstacle obs
+
+prop_post_finDeTourObstacles :: Cadence -> [Obstacle] -> (Cadence, [Obstacle]) -> Bool
+prop_post_finDeTourObstacles _ _ (cad', obs') =
+  prop_inv_cadence cad' && all prop_inv_obstacle obs'
+
+--------------------------------------------------------------------------------
+-- Partie 5 : Ennemis
+--------------------------------------------------------------------------------
+
+data Action = Deplacer Direction | Tirer | Attendre
+  deriving (Eq, Show)
+
+data Oracle
+  = Scripted [Action] Int     -- liste cyclique + index
+  | FromInt                   -- action déterminée par l'int du moteur
+  deriving (Eq, Show)
+
+prop_inv_oracle :: Oracle -> Bool
+prop_inv_oracle (Scripted as i) = not (null as) && i >= 0
+prop_inv_oracle FromInt         = True
+
+oracleStep :: Oracle -> Int -> (Action, Oracle)
+oracleStep o n
+  | not (prop_inv_oracle o) = error "oracleStep: invariant oracle violé"
+oracleStep (Scripted as i) _ =
+  let k = i `mod` length as
+  in (as !! k, Scripted as (i + 1))
+oracleStep FromInt n = (actionFromInt n, FromInt)
+
+prop_pre_oracleStep :: Oracle -> Int -> Bool
+prop_pre_oracleStep o _ = prop_inv_oracle o
+
+prop_post_oracleStep :: Oracle -> Int -> (Action, Oracle) -> Bool
+prop_post_oracleStep _ _ (_a, o') = prop_inv_oracle o'
+
+actionFromInt :: Int -> Action
+actionFromInt n =
+  case abs n `mod` 5 of
+    0 -> Attendre
+    1 -> Tirer
+    2 -> Deplacer Gauche
+    3 -> Deplacer Droite
+    _ -> Deplacer Bas
+
+newtype PV = PV Int
+  deriving (Eq, Show, Ord)
+
+prop_inv_pv :: PV -> Bool
+prop_inv_pv (PV x) = x > 0
+
+mkPV :: Int -> Either Text PV
+mkPV x
+  | x > 0     = Right (PV x)
+  | otherwise = Left ("PV doit être > 0, reçu " <> tshow x)
+
+data Ennemi = Ennemi
+  { eHitbox   :: Hitbox
+  , ePV       :: PV
+  , eOracle   :: Oracle
+  , eCadTir   :: Cadence
+  } deriving (Eq, Show)
+
+prop_inv_ennemi :: Ennemi -> Bool
+prop_inv_ennemi e =
+  prop_inv_hitbox (eHitbox e)
+  && prop_inv_pv (ePV e)
+  && prop_inv_oracle (eOracle e)
+  && prop_inv_cadence (eCadTir e)
+
+mkEnnemi :: Hitbox -> PV -> Oracle -> Cadence -> Either Text Ennemi
+mkEnnemi h pv o cad
+  | prop_inv_hitbox h && prop_inv_pv pv && prop_inv_oracle o && prop_inv_cadence cad
+      = Right (Ennemi h pv o cad)
+  | otherwise = Left "Ennemi invalide (hitbox/PV/oracle/cadence)"
+
+-- fin de tour ennemi: renvoie (ennemi mis à jour, projectile éventuellement créé)
+finDeTourEnnemi :: Int -> Ennemi -> (Ennemi, Maybe Projectile)
+finDeTourEnnemi rnd e
+  | not (prop_inv_ennemi e) = error "finDeTourEnnemi: invariant ennemi violé"
+  | otherwise =
+      let (act, o') = oracleStep (eOracle e) rnd
+          e0 = e { eOracle = o' }
+      in case act of
+          Attendre -> (e0, Nothing)
+          Deplacer d ->
+            let (dx,dy) = dirVector d
+            in (e0 { eHitbox = translateHitbox dx dy (eHitbox e0) }, Nothing)
+          Tirer ->
+            let (shootNow, cad') = tickCadence (eCadTir e0)
+                e1 = e0 { eCadTir = cad' }
+            in if shootNow
+                 then
+                   let hShot = eHitbox e1
+                       p = Projectile hShot Bas (Cadence 1 0) TirEnnemi
+                   in (e1, Just p)
+                 else (e1, Nothing)
+
+prop_pre_finDeTourEnnemi :: Int -> Ennemi -> Bool
+prop_pre_finDeTourEnnemi _ = prop_inv_ennemi
+
+prop_post_finDeTourEnnemi :: Int -> Ennemi -> (Ennemi, Maybe Projectile) -> Bool
+prop_post_finDeTourEnnemi _ _ (e', mp) =
+  prop_inv_ennemi e' && maybe True prop_inv_projectile mp
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,206 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{- HLINT ignore "Use <$>" -}
+
+module Main (main) where
+
+import Lib
+
+import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Hitbox: invariants et constructeurs" $ do
+    it "mkDisque rejette r<=0" $ do
+      mkDisque 0 0 0 `shouldSatisfy` isLeft
+      mkDisque 0 0 (-3) `shouldSatisfy` isLeft
+
+    it "mkRectangle rejette w<=0 ou h<=0" $ do
+      mkRectangle 0 0 0 1 `shouldSatisfy` isLeft
+      mkRectangle 0 0 1 0 `shouldSatisfy` isLeft
+
+    it "mkComposee rejette <2 composantes" $ do
+      mkComposee [] `shouldSatisfy` isLeft
+      mkComposee [Point 0 0] `shouldSatisfy` isLeft
+
+    prop "Les smart constructors produisent une hitbox valide" $
+      \(SmallInt x) (SmallInt y) (Positive r) (Positive w) (Positive h) ->
+        isRightWith prop_inv_hitbox (mkDisque x y r)
+        && isRightWith prop_inv_hitbox (mkRectangle x y w h)
+
+  describe "Hitbox: collision et propriété Composee" $ do
+    it "Exemple simple: composee de 2 points" $ do
+      let p1 = Point 0 0
+          p2 = Point 10 10
+          h1 = Composee [p1,p2]
+      collision h1 (Point 0 0) `shouldBe` True
+      collision h1 (Point 10 10) `shouldBe` True
+      collision h1 (Point 0 10) `shouldBe` False
+
+    prop "Q1.4: si collision avec Composee[2 points], alors c'est un des 2 points" $
+      \(SmallInt x1) (SmallInt y1) (SmallInt x2) (SmallInt y2) (SmallInt x) (SmallInt y) ->
+        prop_composee2points_point (x1,y1) (x2,y2) (x,y)
+    -- au lieu d’utiliser arbitrary, utilise ce générateur
+  describe "Cadence" $ do
+    prop "tickCadence préserve l'invariant" $
+      forAll genCadence $ \c ->
+        let (_, c') = tickCadence c in prop_inv_cadence c'
+
+    prop "tickCadence respecte sa post-condition" $
+      forAll genCadence $ \c ->
+        prop_post_tickCadence c (tickCadence c)
+
+    it "tickCadence fait bouger exactement tous les 'attente' tours" $ do
+      let Right c0 = mkCadence 3
+          moves = take 7 (map fst (iterate (tickCadence . snd) (tickCadence c0)))
+      moves `shouldBe` [True,False,False,True,False,False,True]
+
+  describe "Projectiles" $ do
+    prop "finDeTourProjectile préserve l'invariant" $
+      forAll genProjectile $ \p ->
+        prop_inv_projectile (finDeTourProjectile p)
+
+    prop "finDeTourProjectile respecte sa post-condition" $
+      forAll genProjectile $ \p ->
+        prop_post_finDeTourProjectile p (finDeTourProjectile p)
+
+    it "Un projectile avec cadence=1 avance à chaque tour" $ do
+      let Right c = mkCadence 1
+          Right p0 = mkProjectile (Point 0 0) Haut c TirJoueuse
+          p1 = finDeTourProjectile p0
+          p2 = finDeTourProjectile p1
+      prHitbox p1 `shouldBe` Point 0 1
+      prHitbox p2 `shouldBe` Point 0 2
+
+  describe "Ennemis" $ do
+    it "Oracle Scripted est cyclique" $ do
+      let o0 = Scripted [Attendre, Tirer] 0
+          (a1,o1) = oracleStep o0 123
+          (a2,o2) = oracleStep o1 999
+          (a3,_)  = oracleStep o2 42
+      [a1,a2,a3] `shouldBe` [Attendre, Tirer, Attendre]
+
+    prop "finDeTourEnnemi préserve l'invariant (ennemi)" $
+      forAll genEnnemi $ \e ->
+        let (e', _mp) = finDeTourEnnemi 0 e
+        in prop_inv_ennemi e'
+
+--------------------------------------------------------------------------------
+-- Helpers QuickCheck
+--------------------------------------------------------------------------------
+
+isLeft :: Either a b -> Bool
+isLeft (Left _) = True
+isLeft _        = False
+
+isRightWith :: (b -> Bool) -> Either a b -> Bool
+isRightWith pr (Right x) = pr x
+isRightWith _  _         = False
+
+-- Ça sert à contraindre les valeurs générées (ici: petites valeurs, pour éviter des trucs énormes).
+-- comme Int va jusqu'à 2 millard
+newtype SmallInt = SmallInt Int deriving (Show)
+-- class Arbitrary a where
+--      arbitrary :: Gen a
+instance Arbitrary SmallInt where
+  arbitrary = SmallInt <$> chooseInt (-100, 100) -- chooseInt (-100,100) :: Gen Int (génère un Int dans l’intervalle)
+-- (<$>) c’est fmap:
+-- (<$>) :: (a -> b) -> Gen a -> Gen b
+
+
+genCadence :: Gen Cadence
+genCadence = do
+  Positive a <- arbitrary
+  r <- chooseInt (0, a - 1)
+  pure (Cadence a r)
+
+{-
+    Direction c’est un type avec plusieurs valeurs possibles:
+    Haut, Bas, Gauche, Droite
+    [minBound .. maxBound] = liste de toutes les directions
+    elements = “choisis un élément au hasard dans la liste”
+    Donc:
+    QuickCheck choisit une direction au hasard.
+-}
+instance Arbitrary Direction where
+  arbitrary :: Gen Direction
+  arbitrary = elements [minBound .. maxBound]
+
+instance Arbitrary ProjectileOwner where
+  arbitrary = elements [minBound .. maxBound]
+
+{-
+    Le n (une limite de profondeur)
+    si on ne limites pas, QuickCheck peut créer:
+    Composee [Composee [Composee [...]]] à l’infini.
+    Donc n sert de frein.
+
+    2 => On choisit:
+
+    6 fois plus souvent une hitbox simple
+    1 fois sur 7 une hitbox composée”
+    frequency = “tirage au sort pondéré”.
+
+    oneof = “choisis une option au hasard”.
+    Ici les options: Point, Disque ,Rectangle
+
+    composee
+
+    k <- chooseInt (2,4) combien de sous-hitbox il y aura (2 à 4)
+    2 minimum Parce que invariant: Composee doit contenir au moins 2 hitbox.
+    hs <- vectorOf k (genHitbox (n div 2)) génère k hitbox, chacune avec une profondeur réduite
+
+    n div 2
+    Pour que ça s’arrête rapidement (la profondeur diminue).
+-}
+genHitbox :: Int -> Gen Hitbox
+genHitbox n
+  | n <= 0 = base
+  | otherwise = frequency
+      [ (6, base)
+      , (1, composee)
+      ]
+  where
+    base = oneof
+      [ Point <$> chooseInt (-200,200) <*> chooseInt (-200,200)
+      , do xc <- chooseInt (-200,200)
+           yc <- chooseInt (-200,200)
+           Positive r <- arbitrary
+           pure (Disque xc yc r)
+      , do x <- chooseInt (-200,200)
+           y <- chooseInt (-200,200)
+           Positive w <- arbitrary
+           Positive h <- arbitrary
+           pure (Rectangle x y w h)
+      ]
+    composee = do
+      k <- chooseInt (2,4)
+      hs <- vectorOf k (genHitbox (n `div` 2))
+      pure (Composee hs)
+
+{-
+    QuickCheck nous donne un paramètre de taille automatique (appelé "size").
+    sized genHitbox lui dit: Utilise ce paramètre comme n pour limiter la profondeur.
+-}
+instance Arbitrary Hitbox where
+  arbitrary = sized genHitbox
+
+genProjectile :: Gen Projectile
+genProjectile = do
+  h <- arbitrary
+  d <- arbitrary
+  c <- genCadence
+  o <- arbitrary
+  pure (Projectile h d c o)
+
+genEnnemi :: Gen Ennemi
+genEnnemi = do
+  h <- arbitrary
+  Positive pv <- arbitrary
+  cad <- genCadence
+  let o = Scripted [Attendre, Tirer, Deplacer Gauche, Deplacer Droite] 0
+  pure (Ennemi h (PV pv) o cad)
